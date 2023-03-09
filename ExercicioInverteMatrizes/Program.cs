94e9a22f5a93df8b50cf475444702b3134b2dfb3
GBrandao04 <gabrielmartbrandao@gmail.com>
2023-03-09 03:12:26 -0300

    Add project files.

---
+++ ExercicioInverteMatrizes/Program.cs
@@ -0,0 +1,43 @@
+﻿int[,] first = new int[4, 4];
+int[,] reverse = new int[4, 4];
+
+int row, col;
+
+for(row = 0; row < 4; row++)
+{
+    for(col = 0; col < 4; col++)
+    {
+        first[row, col] = RandomNumber();
+    }
+}
+
+for (row = 0; row < 4; row++)
+{
+    for (col = 0; col < 4; col++)
+    {
+        reverse [3 - row, 3 - col]= first[row, col];
+    }
+}
+
+PrintMatriz(first);
+PrintMatriz(reverse);
+
+void PrintMatriz(int[,] matriz)
+{
+    Console.WriteLine();
+    for (row = 0; row < 4; row++)
+    {
+        for (col = 0; col < 4; col++)
+        {
+            Console.Write($"{matriz[row, col], 3} |");
+        }
+        Console.WriteLine();
+    }
+}
+
+int RandomNumber()
+{
+    Random random = new Random();
+    int rnd = random.Next(100);
+    return rnd;
+}
